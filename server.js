d319198837ec71c17576bce99059d7e294de86f9
Eoneternal33 <74932407+Eoneternal33@users.noreply.github.com>
2023-01-03 18:55:14 -0700

    created server js and started project

---
+++ server.js
@@ -0,0 +1,14 @@
+// re-learnign Node.js and starting with terminal operations
+console.log('Hello World')
+
+// call node server in a new terminal to run the consoles in the terminal 
+// CommonJS import which needs a require statement/ 'os' stands for Operating System
+const os = require('os')
+
+// getting infor about the 'os' that was required
+console.log(os.type())
+console.log(os.version())
+// homedir = Home Directory
+console.log(os.homedir())
+
+
